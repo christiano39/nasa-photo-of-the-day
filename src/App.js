028ca4fc68628cce0ea64beaaea7a3dbefe85198
christiano39 <carneson39@gmail.com>
2020-06-09 20:58:14 -0600

    npm installed axios and created empty component files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import React, { useState, useEffect } from "react";
 import "./App.css";
 import { apiKey } from './APIKey'
+import axios from 'axios'
 
 function App() {
-  const [data, setData] = useState()
+  const [potd, setPOTD] = useState()
 
   useEffect(() => {
-    console.log(apiKey)
+    // axios
+    //   .get(`https://api.nasa.gov/planetary/apod?api_key=${apiKey}`)
+    //   .then(response => {
+    //     console.log(response)
+    //     setPOTD(response.data)
+    //   })
+    //   .catch(err => {
+    //     console.log(`Error: ${err}`)
+    //   })
   }, [])
   
   return (
     <div className="App">
-      
+      <h1>NASA Astronomical Photo of the Day</h1>
     </div>
   );
 }
